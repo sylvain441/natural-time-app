4044adc556746be807a7772bd318e90b76a3430a
sylvain 441 <sylvain441@pm.me>
2025-01-05 22:47:00 +0100

    rethink the router with a better order and handling of the old v1 utm_source=web_app_manifest pwa start_url

--- src/router/router.js
+++ src/router/router.js
@@ -16,6 +16,18 @@ const routes = [
     path: '/fr/', 
     name: 'welcome', 
     component: WelcomeView,
+    beforeEnter: (to, from, next) => {
+      // if PWA is saved with the old v1 start_url scheme, redirect to time view (or welcome view as fallback)
+      if (to.fullPath.includes('utm_source=web_app_manifest')) {
+        const contextStore = useContextStore()
+        next(contextStore.storedLatitude && contextStore.storedLongitude 
+          ? { name: 'time' }
+          : { name: 'welcome' }
+        )
+      } else {
+        next()
+      }
+    }
   },
   { 
     path: '/fr/horloge-temps-naturel', 
@@ -27,26 +39,8 @@ const routes = [
     name: '13moons', 
     component: SpiralView,
   },
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'not-found',
-    component: NotFoundView,
-  },
-  // OLD ROUTES
   { 
-    path: '/:latlng?/:location?', 
-    redirect: { name: 'time' }
-  },
-  { 
-    path: '/13moons/:latlng?/:location?', 
-    redirect: { name: '13moons' }
-  },
-  { 
-    path: '/about', 
-    redirect: { name: 'welcome' }
-  },
-  { 
-    path: '/startpwa',
+    path: '/startpwa/', 
     name: 'startpwa',
     redirect: to => {
       const contextStore = useContextStore()
@@ -54,6 +48,32 @@ const routes = [
         ? { name: 'time' }
         : { name: 'welcome' }
     }
+  },
+  // OLD ROUTES
+  { 
+    path: '/about', 
+    redirect: { name: 'welcome' }
+  },
+  { 
+    path: '/13moons/:latlng/:location?', 
+    redirect: { name: '13moons' }
+  },
+  { 
+    path: '/:latlng/:location?',
+    beforeEnter: (to, from, next) => {
+      alert(to.params.latlng)
+      const latlngRegex = /^[0-9.,]+$/
+      if (latlngRegex.test(to.params.latlng)) {
+        next({ name: 'time' })
+      } else {
+        next({ name: 'not-found' })
+      }
+    }
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: NotFoundView,
   },
 ]
 
